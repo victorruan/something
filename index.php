ea5a59ab6c92e06d8787a80065d12f1bb9caf1e3
阮佳佳 <250069802@qq.com>
2016-07-08 13:23:54 +0800

    ~

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+    <meta http-equiv="content-type" content="text/html;charset=utf-8">
+    <title>&amp;nbsp;如何在网页展现</title>
+</head>
+<body>
+<?=  "&nbsp;"; //不会显示 ?>
+<?=  htmlentities("&nbsp;");//会显示 ?>
+</body>
+</html>
